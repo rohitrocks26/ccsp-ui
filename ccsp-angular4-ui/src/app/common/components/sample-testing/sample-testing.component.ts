d9de92bc30262e8dd588b1a39dcc65690fa79280
US\ychandra <ychandra@deloitte.com>
2017-10-04 19:00:54 +0530

    Component with Sample Testing Added

---
+++ ccsp-angular4-ui/src/app/common/components/sample-testing/sample-testing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-testing',
+  templateUrl: './sample-testing.component.html',
+  styleUrls: ['./sample-testing.component.css']
+})
+export class SampleTestingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
