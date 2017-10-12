e402ca89ed03766e558746a4ae0845f5d7683680
Khaitan <adkhaitan@deloitte.com>
2017-10-12 16:48:51 +0530

    demoPage spec update

--- ccsp-angular4-ui/src/app/demo-page/demo-page.component.spec.ts
+++ ccsp-angular4-ui/src/app/demo-page/demo-page.component.spec.ts
@@ -1,7 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA} from '@angular/core'
 import { DemoPageComponent } from './demo-page.component';
-
+import { FormsModule }   from '@angular/forms';
+import {GlobalService} from '../common/services/global.service';
+import {AuthenticationService} from '../common/services/authentication.service';
+import { HttpModule } from '@angular/http';
 describe('DemoPageComponent', () => {
   let component: DemoPageComponent;
   let fixture: ComponentFixture<DemoPageComponent>;
@@ -9,7 +12,9 @@ describe('DemoPageComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ DemoPageComponent ],
-      schemas : [NO_ERRORS_SCHEMA]
+      schemas : [NO_ERRORS_SCHEMA],
+      imports:[FormsModule,HttpModule],
+      providers:[GlobalService,AuthenticationService]
     })
     .compileComponents();
   }));
