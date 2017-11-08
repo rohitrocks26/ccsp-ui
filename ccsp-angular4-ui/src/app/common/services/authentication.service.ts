a47cdb99abc1a40d1ded41f56ea5f85ac0244623
US\ychandra <ychandra@deloitte.com>
2017-11-08 12:37:35 +0530

    Authentication Service hooked to backend

--- ccsp-angular4-ui/src/app/common/services/authentication.service.ts
+++ ccsp-angular4-ui/src/app/common/services/authentication.service.ts
@@ -1,5 +1,6 @@
 import { Injectable,Component } from '@angular/core';
 import { Http, Response, RequestOptions, Headers, Request, RequestMethod} from '@angular/http';
+import {Router} from '@angular/router';
 import {Observable} from 'rxjs/Rx';
 import { Constants } from '../constants';
 import 'rxjs/Rx';
@@ -8,7 +9,7 @@ import 'rxjs/Rx';
 export class AuthenticationService {
     private _authToken : string;
     private headers : Headers;
-    constructor(private httpClient : Http) {
+    constructor(private httpClient : Http, private router : Router ) {
         this.headers = this.getHeaders();
     }
     public get accessToken() : string {
@@ -18,20 +19,25 @@ export class AuthenticationService {
         return localStorage.getItem('token') !== null ? true : false;
     }
     public authenticate( username : string, password : string ) {
-        let detail = JSON.stringify({ username : username, password : password })
         this.httpClient
-        .post(Constants._authenticationUrl, detail)
-        .subscribe(response => this.handleResponse(response.text()))
+        .post(Constants._authenticationUrl, { username : username, password : password })
+        .subscribe(response => this.handleResponse(response))
     }
     public handleResponse(response) {
-        localStorage.setItem('token', response)
-        console.log(localStorage.getItem('token'));
+        //Subject to change depending upon the token response returned
+        if(response._body !== undefined) { 
+            localStorage.setItem('token', response._body);
+            this.router.navigate(['/memberInquiry']);
+        }
+        localStorage.setItem('token', response._body);
+        //console.log(response._body);
         // Pending -- Save User information as well
     }
     
     private getHeaders () : Headers {
         let header = new Headers();
         header.set('Access-Control-Allow-Origin','*');
+        header.set('Content-type','application/json');
         return header;
     }
 
