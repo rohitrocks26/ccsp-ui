135576ca45c313a028fe432be8971a95fac2bc29
Khaitan <adkhaitan@deloitte.com>
2017-11-09 12:43:09 +0530

    code clean up

--- ccsp-angular4-ui/src/app/common/model/Member.ts
+++
@@ -1,7 +0,0 @@
-export class Member {
-constructor(public name:string , public partyId:string, public subscriberId?:string){
-    this.name=name;
-    this.partyId = partyId;
-    this.subscriberId = subscriberId;
-  }
-}
